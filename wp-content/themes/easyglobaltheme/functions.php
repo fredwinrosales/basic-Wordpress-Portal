fd61648c5db7f2f14fb7dfe397aa03238753c1e3
Fredwin Rosales <fredwinrosales@gmail.com>
2017-07-11 23:40:23 -0400

    Agregando style.css a function.php
    Se agrega style.css desde function.php

--- wp-content/themes/easyglobaltheme/functions.php
+++ wp-content/themes/easyglobaltheme/functions.php
@@ -11,6 +11,7 @@ function easyglobaltheme_script_enqueue(){
 
     wp_enqueue_style('bootstrap', get_template_directory_uri() . '/css/bootstrap/bootstrap.min.css', array(), '3.3.7', 'all');
     wp_enqueue_style('customstyle', get_template_directory_uri() . '/css/easyglobaltheme.css', array(), '1.0.0', 'all');
+    wp_enqueue_style('style', get_template_directory_uri() . '/style.css', array(), '1.0.0', 'all');
     wp_enqueue_script('jquery');
     wp_enqueue_script('bootstrapjs', get_template_directory_uri() . '/js/bootstrap/bootstrap.min.js', '3.3.7');
     wp_enqueue_script('customjs', get_template_directory_uri() . '/js/easyglobaltheme.js', '1.0.0', true);
