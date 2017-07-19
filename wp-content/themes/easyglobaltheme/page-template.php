e49a81b96941df7a618ab44faa74dad2ba0c3b01
Fredwin Rosales <fredwinrosales@gmail.com>
2017-07-18 21:59:40 -0400

    page-template
    Se crea nueva plantilla para usarla en la creación de nuevas páginas.

---
+++ wp-content/themes/easyglobaltheme/page-template.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+    Template Name: Page No Title
+ */
+
+get_header();
+?>
+
+<?php if( have_posts() ):
+
+    while( have_posts() ): the_post(); ?>
+
+        <h3>This is my static title</h3>
+
+        <small>Post on: <?php the_time('F j, Y'); ?> at <?php the_time('g:i a')?>, in <?php the_category(); ?></small>
+
+        <p><?php the_content(); ?></p>
+
+    <?php endwhile;
+
+endif;
+
+?>
